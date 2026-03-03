f65b21f59adf5a615dcf34ab192138cecba85c08
Rae <aldrichpadilla404@gmail.com>
2026-03-03 22:12:45 +0800

    I created a class called GitRepository and I honestly don't know what I am doing.

--- libwyag.py
+++ libwyag.py
@@ -17,4 +17,52 @@ argparser = argparse.ArgumentParser(description="The stupidest content tracker")
 argsubparser = argparser.add_subparsers(title="Commands", dest="command")
 argsubparser.required = True
 
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add"          : cmd_add(args)
+        case "cat_file"     : cmd_cat_file(args)
+        case "check-ignore" : cmd_check_ignore(args)
+        case "checkout"     : cmd_checkout(args)
+        case "commit"       : cmd_commit(args)
+        case "hash-object"  : cmd_hash_object(args)
+        case "init"         : cmd_init(args)
+        case "log"          : cmd_log(args)
+        case "ls-files"     : cmd_ls_files(args)
+        case "ls-tree"      : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        case "rm"           : cmd_rm(args)
+        case "show-ref"     : cmd_show_ref(args)
+        case "status"       : cmd_status(args)
+        case "tag"          : cmd_tag(args)
+        case _              : print("Bad command.")
 
+class GitRepository (object):
+    #a git repository
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+def __init__(self, path, force=False):
+    self.worktree = path
+    self.gitdir = os.path.join(path, ".git")
+
+    if not (force or os.path.isdir(self.gitdir)):
+        raise Exception(f"Not a Git Repository {path}")
+    
+    #read configuration file in .git/config
+    self.conf = configparser.ConfigParser()
+    cf = repo_file(self, "config")
+
+    if cf and os.conf.exists(cf):
+        self.conf.read([cf])
+    elif not force:
+        raise Exception("Configuration file missing")
+    
+    if not force:
+        vers = int(self.conf.get("core", "repositoryformatversion"))
+        if vers != 0:
+            raise Exception(f"Unsupported repositoryformatversion: {vers}")
+        
+    
