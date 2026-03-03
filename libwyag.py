7208fef8a656e44ff221d20d1db9b2bf1240bba7
Rae <aldrichpadilla404@gmail.com>
2026-03-03 22:29:31 +0800

    taking a break :)

--- libwyag.py
+++ libwyag.py
@@ -38,8 +38,8 @@ def main(argv=sys.argv[1:]):
         case _              : print("Bad command.")
 
 class GitRepository (object):
-    #a git repository
-
+    """A git repository"""
+    
     worktree = None
     gitdir = None
     conf = None
@@ -64,5 +64,36 @@ def __init__(self, path, force=False):
         vers = int(self.conf.get("core", "repositoryformatversion"))
         if vers != 0:
             raise Exception(f"Unsupported repositoryformatversion: {vers}")
+        
+def repo_path(repo, *path):
+    """Compute path under repo's gitdir"""
+    return os.path.join(repo.gitdir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+    """Same as repo_path, but create dirname(*path) if absent.  For
+example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
+.git/refs/remotes/origin."""
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir."""
+
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception(f"File not directory {path}")
+        
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+    
+
         
     
